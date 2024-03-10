f4aceb26019cd9ba8c07b37a883eff6c36d4ca6b
wlldxn <william.dixon.home@gmail.com>
2024-03-10 13:22:53 +0000

    Complete sort_nosort testing

--- graphs_merge_sort.py
+++ graphs_merge_sort.py
@@ -28,7 +28,7 @@ def get_data(fname):
     print(df)
     df["method"] = df["method"].str.replace("times.", "", regex=False) + df['implementation']
     df['method'] += 'nosort' if ('nosort') in fname else 'sort'
-    # df.drop(df[df.method.str.contains('p')].index, inplace=True)
+    df.drop(df[df.method.str.contains('lsd')].index, inplace=True)
     df = df.dropna()
     return df
 
@@ -105,15 +105,16 @@ def g(df, fname):
 
 if __name__ == '__main__':
     data=[]
-    data.append(get_data(('/home/will/dissertation/sort_times/workingfinal_workingfinalnosort_3.json')))
+    data.append(get_data(('/home/will/dissertation/sort_times/sort_comparison.json')))
+    data.append(get_data(('/home/will/dissertation/sort_times/workingfinal_1.json')))
     # data.append(get_data(('/home/will/dissertation/sort_times/workingfinal_0.json')))
     # data.append(get_data(('/home/will/dissertation/sort_times/insertion_evident_0.json')))
     # for i in [12]:
     #     fname = Path('/home/will/dissertation/sort_times') / f'fewer_iters_insertion_evident_timsort_{i}.json'
     #     data.append(get_data(fname))
     gdata = pd.concat(data)
-    print(gdata)
-    g(gdata, 'workingfinal_workingfinalnosort_3')
+    # print(gdata)
+    g(gdata, 'workingfinal_sortcomparison')
     # get_data(Path('/home/will/dissertation/sort_times/fewer_iters_insertion_evident_timsort_11.json'))
     # do_graph(reject_outliers, Path('/home/will/dissertation/sort_times/fewer_iters_insertion_evident_timsort_11.json'))
     # for i in range(3,4):
